393fafb519de1194bc5ac39c56e5ca732b11c7f2
Peachesmoon <132327956+Peachesmoon@users.noreply.github.com>
2023-12-29 20:38:50 +0800

    site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Peach_Blog',
+  domain: 'blog.peaches.eu.org',
+  author: 'WelsonPeach',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
