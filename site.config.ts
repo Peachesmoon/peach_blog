68f7398c3c8bae19342a9ec48d6b21f1b15ea6cc
Peachesmoon <132327956+Peachesmoon@users.noreply.github.com>
2024-08-16 20:51:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Peach'sBlog',
+  name: 'Peach s Blog',
   domain: 'blog.peaches.eu.org',
   author: 'WelsonPeach',
 
